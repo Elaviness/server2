e3af2d29cf6a988f3862cbf7c1700cf67f4d8d44
Elaviness <48585255+Elaviness@users.noreply.github.com>
2019-07-09 20:41:24 +0300

    fix default in models3

--- practice/models.py
+++ practice/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 class GeoData(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     GPSstring = models.CharField(max_length=70, default='', null=True)
 
     GPStime = models.BigIntegerField(default=0, null=True)
